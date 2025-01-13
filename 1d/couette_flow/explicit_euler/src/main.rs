2df8cbec18b3217e62510060c2754dd626a8e785
azizka85 <aziz.kudaikulov@gmail.com>
2025-01-13 20:43:34 +0500

    Refactor filters and sources for analytical solution

--- 1d/couette_flow/explicit_euler/src/main.rs
+++ 1d/couette_flow/explicit_euler/src/main.rs
@@ -1,4 +1,5 @@
-use std::{fs, io::{self, Write}};
+use std::fs;
+use std::io::{self, Write};
 
 const NU: f64 = 1.;
 const U: f64 = 1.;
@@ -6,19 +7,13 @@ const U: f64 = 1.;
 const H: f64 = 1.;
 const T: f64 = 0.3;
 
-const M: usize = 100;
+const OTS: f64 = 0.003;
 
 const DIR: &str = "data/1d/couette_flow/explicit_euler";
 
 fn main() {        
     let dy = 0.01;
-    let n = (H/dy).ceil() as usize + 1;
-
-    if n == 0 {
-        println!("Error: n = 0");
-
-        return;
-    }
+    let ny = (H/dy).ceil() as usize + 1;
 
     let r = 0.3; 
     let dt = r*dy*dy/NU;   
@@ -32,39 +27,46 @@ fn main() {
         Ok(path) => path
     };
     
-    let mut u = vec![0.; n];
-    let mut u1 = vec![0.; n];
+    let mut u = vec![0.; ny];
+    let mut u1 = vec![0.; ny];
 
     set_initial_condition(&mut u);
     set_boundary_condition(&mut u);
 
     let mut t = 0.;
-    let mut m = 0;
+    let mut tn = OTS;
 
-    if let Err(err) = write_file(&u, t, dy, m, &path) {
-        println!("Error in writing file: m={m}, t={t}: {:?}", err);
+    let mut n = 0;    
+
+    if let Err(err) = write_file(&u, t, dy, n, &path) {
+        println!("Error in writing file: n={n}, t={t}: {:?}", err);
 
         return;
+    } else {
+        n += 1;
     }
     
     while t <= T {
         set_boundary_condition(&mut u1);
 
-        for i in 1..n-1 {
+        for i in 1..ny-1 {
             u1[i] = u[i] + r*(u[i+1] - 2.*u[i] + u[i-1]);
         }
 
         update_data(&mut u, &mut u1);
 
-        t += dt;
-        m += 1;
+        t += dt;                        
 
-        if m % M == 0 {
-            if let Err(err) = write_file(&u, t, dy, m, &path) {
-                println!("Error in writing file: m={m}, t={t}: {:?}", err);
+        if t >= tn {
+            if let Err(err) = write_file(&u, t, dy, n, &path) {
+                println!("Error in writing file: n={n}, t={t}: {:?}", err);
         
                 return;
+            } else {
+                n += 1;
             }
+
+            tn += OTS;
         }
     } 
 }
@@ -77,20 +79,20 @@ fn set_environment(dy: f64, r: f64) -> io::Result<String> {
     Ok(path)
 }
 
-fn write_file(u: &Vec<f64>, t: f64, dy: f64, m: usize, path: &str) -> io::Result<()> {
-    let mut file = fs::File::create(format!("{path}/data.{m}.vtk"))?;
+fn write_file(u: &Vec<f64>, t: f64, dy: f64, n: usize, path: &str) -> io::Result<()> {
+    let mut file = fs::File::create(format!("{path}/data.{n:03}.vtk"))?;
 
     writeln!(file, "# vtk DataFile Version 3.0")?;
     writeln!(file, "TIME {t:.3}")?;
     writeln!(file, "ASCII")?;
     writeln!(file, "DATASET STRUCTURED_GRID")?;
 
-    let n = u.len();
+    let ny = u.len();
 
-    writeln!(file, "DIMENSIONS 1 {n} 1")?;
-    writeln!(file, "POINTS {n} float")?;
+    writeln!(file, "DIMENSIONS 1 {ny} 1")?;
+    writeln!(file, "POINTS {ny} float")?;
 
-    for i in 0..n {
+    for i in 0..ny {
         let y = dy * i as f64;
 
         writeln!(file, "0.0 {y:.3} 0.0")?;
@@ -100,11 +102,11 @@ fn write_file(u: &Vec<f64>, t: f64, dy: f64, m: usize, path: &str) -> io::Result
     writeln!(file, "Time 1 1 float")?;
     writeln!(file, "{t:.3}")?;
 
-    writeln!(file, "POINT_DATA {}", n)?;
+    writeln!(file, "POINT_DATA {}", ny)?;
     writeln!(file, "SCALARS u float")?;
     writeln!(file, "LOOKUP_TABLE default")?;
 
-    for i in 0..n {
+    for i in 0..ny {
         writeln!(file, "{:.3}", u[i])?;
     } 
 
--- post_processing/1d/couette_flow/filter_analytical_solution_structured_grid.py
+++ post_processing/1d/couette_flow/filter_analytical_solution_structured_grid.py
@@ -1,7 +1,9 @@
-U = 1
-H = 1
+import numpy as np
 
-nu = 1
+U = 1.
+H = 1.
+
+nu = 1.
 
 y = inputs[0].Points[:,1]
 t = inputs[0].FieldData['Time']
@@ -11,4 +13,5 @@ u = U*y/H
 for n in range(1,11):
     u += 2*U*exp(-n**2*np.pi**2*nu*t/H**2)*cos(np.pi*n)*sin(np.pi*n*y/H)/np.pi/n
 
-return u
+output.PointData.append(u, f'u10')
+output.PointData.append(inputs[0].PointData['u'], f'u')
