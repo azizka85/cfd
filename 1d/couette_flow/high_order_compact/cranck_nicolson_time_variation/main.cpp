80b7ad0f221c3f27175dc90301010f94e94db748
azizka85 <aziz.kudaikulov@gmail.com>
2025-06-12 11:39:23 +0500

    implement 4th order compact fd scheme

---
+++ 1d/couette_flow/high_order_compact/cranck_nicolson_time_variation/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+#include "solver.h"
+
+using namespace std;
+
+using namespace CouetteFlow::HighOrder::Compact::CranckNicolsonWithTimeVariation;
+
+int main() {
+    double uTop = 1;
+    double nu = 1;
+    double h = 1;
+    double dy = 0.01;
+    double r = 0.5;
+    double b = 1.1;
+    double endTime = 0.3;
+    double outputTimeStep = 0.003;
+
+    string dir = "data";
+
+    try {
+        Solver solver(uTop, nu, h, dy, r, b, endTime, outputTimeStep, dir);
+
+        solver.solve();
+    } catch (const exception& e) {
+        cout << "Caught exception: " << e.what() << std::endl;
+    }
+
+    return 0;
+}
